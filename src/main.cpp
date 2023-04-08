9aa42f6625578e94619ea3467c86126d82ac409a
Mátyás Sándor <matyas.sandor@finecutbodies.com>
2023-04-08 17:06:21 +0200

    Implement joystick prototype

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,20 @@
 
 static constexpr int s_debounceIntervalMs = 5;
 
+static uint32_t lastJoystickX = 0;
+static uint32_t lastJoystickY = 0;
+
 Bounce button_1;
+
+static constexpr int s_joystickXPin = 14;
+static constexpr int s_joystickYPin = 15;
 
 void setup() {
   button_1.attach(2, INPUT_PULLUP);
   button_1.interval(s_debounceIntervalMs);
+
+  pinMode(s_joystickXPin, INPUT);
+  pinMode(s_joystickYPin, INPUT);
 
   Serial.begin(115200);
   while (!Serial && millis() < 5000)
@@ -25,5 +34,14 @@ void loop() {
 
   if (button_1.rose()) {
     Serial.println("Button is released");
+  }
+
+  const auto joystickX = analogRead(s_joystickXPin) / 8;
+  const auto joystickY = analogRead(s_joystickYPin) / 8;
+
+  if (joystickX != lastJoystickX || joystickY != lastJoystickY) {
+    Serial.printf("X: %d, Y: %d\n", joystickX, joystickY);
+    lastJoystickX = joystickX;
+    lastJoystickY = joystickY;
   }
 }
