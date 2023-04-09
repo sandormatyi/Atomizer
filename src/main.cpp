a7e465a6b5936e5a2b360890e0dcb3b2d8c8c3f6
Mátyás Sándor <matyas.sandor@finecutbodies.com>
2023-04-09 09:44:40 +0200

    Add startup sequence, refactor joystick handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,115 @@
 #include <Arduino.h>
+#include <MIDI.h>
 #include <array>
 #include "Bounce2.h"
 
+#define PIN_BUTTON_1 2
+#define PIN_JOYSTICK_X 15
+#define PIN_JOYSTICK_Y 14
+
 static constexpr int s_debounceIntervalMs = 5;
 
-static uint32_t lastJoystickX = 0;
-static uint32_t lastJoystickY = 0;
+static int joystickCenterX = 0;
+static int joystickCenterY = 0;
 
 static std::array<Bounce, 10> buttons;
+constexpr std::array<uint8_t, 10> s_buttonsToMidiNotes {40, 41, 43, 45, 46, 44, 42, 37, 39, 38};
 
-static constexpr int s_joystickXPin = 15;
-static constexpr int s_joystickYPin = 14;
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
+constexpr int s_midiChannel = 6;
 
-void setup() {
-  for (auto i = 0; i < buttons.size(); ++i) {
+void playStartupSequence()
+{
+  for (size_t i = 0; i < buttons.size(); ++i) {
+    pinMode(PIN_BUTTON_1 + i, OUTPUT_OPENDRAIN);
+    delayMicroseconds(1000*100);
+  }
+
+  for (size_t i = 0; i < buttons.size(); ++i) {
+    pinMode(PIN_BUTTON_1 + i, INPUT_PULLUP);
+    delayMicroseconds(1000*100);
+  }
+}
+
+uint8_t getAdjustedJoystickValue(int rawValue, int centerValue)
+{
+  constexpr uint8_t s_targetCenterValue = 64;
+  constexpr int s_joystickCenterZone = 4;
+
+    if (abs(rawValue - centerValue) <= s_joystickCenterZone) {
+      return s_targetCenterValue;
+    } else if (rawValue < centerValue) {
+      const auto normalizedValue = (rawValue * s_targetCenterValue / centerValue);
+      return uint8_t(max(normalizedValue, 0));
+    } else {
+      const auto normalizedValue = (rawValue - centerValue) * s_targetCenterValue / (1023 - centerValue) + 64;
+      return uint8_t(min(normalizedValue, 127));
+    }
+}
+
+void setup()
+{
+  playStartupSequence();
+
+  for (size_t i = 0; i < buttons.size(); ++i) {
     buttons[i].attach(2 + i, INPUT_PULLUP);
     buttons[i].interval(s_debounceIntervalMs);
   }
 
-  pinMode(s_joystickXPin, INPUT);
-  pinMode(s_joystickYPin, INPUT);
+  pinMode(PIN_JOYSTICK_X, INPUT);
+  pinMode(PIN_JOYSTICK_Y, INPUT);
+
+  delayMicroseconds(1000);
+
+  joystickCenterX = analogRead(PIN_JOYSTICK_X);
+  joystickCenterY = analogRead(PIN_JOYSTICK_Y);
 
   Serial.begin(115200);
   while (!Serial && millis() < 5000)
     ;
 
-  Serial.println("Hello world!");
+  Serial.printf("Joystick center values\nX: %d, Y: %d\n", joystickCenterX, joystickCenterY);
+
+  MIDI.begin();
 }
 
-void loop() {
-  for (auto i = 0; i < buttons.size(); ++i) {
+void loop()
+{
+  for (size_t i = 0; i < buttons.size(); ++i) {
     buttons[i].update();
 
     if (buttons[i].fell()) {
       Serial.printf("Button %d is pressed\n", i);
+
+      MIDI.sendNoteOn(s_buttonsToMidiNotes[i], 127, s_midiChannel);
     }
 
     if (buttons[i].rose()) {
       Serial.printf("Button %d is released\n", i);
+
+      MIDI.sendNoteOff(s_buttonsToMidiNotes[i], 127, s_midiChannel);
     }
   }
 
-  const auto joystickX = analogRead(s_joystickXPin) / 8;
-  const auto joystickY = analogRead(s_joystickYPin) / 8;
+  const auto joystickX = analogRead(PIN_JOYSTICK_X);
+  const auto joystickY = analogRead(PIN_JOYSTICK_Y);
 
-  if (joystickX != lastJoystickX || joystickY != lastJoystickY) {
-    Serial.printf("X: %d, Y: %d\n", joystickX, joystickY);
-    lastJoystickX = joystickX;
-    lastJoystickY = joystickY;
+  const auto adjustedX = getAdjustedJoystickValue(joystickX, joystickCenterX);
+  const auto adjustedY = getAdjustedJoystickValue(joystickY, joystickCenterY);
+
+  static uint8_t lastJoystickX = 0;
+  static uint8_t lastJoystickY = 0;
+
+  if (adjustedX != lastJoystickX) {
+    lastJoystickX = adjustedX;
+
+    Serial.printf("X CC: %d\n", adjustedX);
+    MIDI.sendControlChange(77, adjustedX, s_midiChannel);
+  }
+
+  if (adjustedY != lastJoystickY) {
+    lastJoystickY = adjustedY;
+
+    // TODO
   }
 }
